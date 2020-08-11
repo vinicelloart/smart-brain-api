b787c3112916b5dbf63b5d1cb5eb845c003a42df
vinicelloart <61485853+vinicelloart@users.noreply.github.com>
2020-08-11 14:33:57 -0600

    updating database

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ const image = require('./controllers/image');
 const db = knex({
  client: 'pg',
  connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-globular-20751',
     user : 'postgres',
-    password : 'test',
+    password : '',
     database : 'smartbrain2'
   }
 });
